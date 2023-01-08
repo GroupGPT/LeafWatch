2ac0334b3ad85da6d6f28b11f16f7a85d7e32b7a
Devin <dheadrick1618@gmail.com>
2023-01-08 06:51:50 -0700

    User auth setup
    Not yet tested. No fake user acc made yet.

--- LeafWatch/sensors/urls.py
+++ LeafWatch/sensors/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.allSensors),
+    # path('', views.allSensors),
+    path('', views.index, name="index"),
     path('<str:GroupSensor_name>/', views.data),
     path('chartTest', views.chartTest, name="chart-test")
 ]
--- LeafWatch/sensors/views.py
+++ LeafWatch/sensors/views.py
@@ -5,6 +5,7 @@ from .models import SensorGroup, Sensor
 from django.template import loader
 
 
+# Not used ??????????????
 def graphs(request, GroupSensor_id):
     sensors = Sensor.objects.all().filter(sensorGroup=GroupSensor_id)
     template = loader.get_template("test/src/graphs.html")
@@ -31,6 +32,26 @@ def allSensors(request):
         'allSensors': allSensors,
     }
     return HttpResponse(template.render(context, request))
+
+
+def index(request):
+    """View function for home page of site."""
+    # allSensors = SensorGroup.objects.all()
+    # template = loader.get_template("sensors/boxes.html")
+    # context = {
+    #     'allSensors': allSensors,
+    # }
+
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
+    context = {
+        'num_visits': num_visits
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
 
 
 def chartTest(request):
