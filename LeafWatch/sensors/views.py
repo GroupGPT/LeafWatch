ab9a1bfadc51e1cec0580f959b488f02ef0dd6e6
Devin <dheadrick1618@gmail.com>
2023-01-08 05:34:05 -0700

    ChartJS working now
    ChartJS working now with a mock chartTest view in the sensors template. Still using random data.
    
    NODE PACKAGES NOT NEEDED ANYMORE . USING CDN IN SCRIPT

--- LeafWatch/sensors/views.py
+++ LeafWatch/sensors/views.py
@@ -1,22 +1,39 @@
+
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import SensorGroup, Sensor
 from django.template import loader
 
 
-def data(request,GroupSensor_name):
-    sensors = Sensor.objects.all().filter(sensorGroup = GroupSensor_name)
-    template = loader.get_template("sensors/data.html")
+def graphs(request, GroupSensor_id):
+    sensors = Sensor.objects.all().filter(sensorGroup=GroupSensor_id)
+    template = loader.get_template("test/src/graphs.html")
     context = {
-        'allSensors' : sensors,
+        'allSensors': sensors,
+        'sensorGroup': GroupSensor_id,
     }
     return HttpResponse(template.render(context, request))
-    
+
+
+def data(request, GroupSensor_id):
+    sensors = Sensor.objects.all().filter(sensorGroup=GroupSensor_id)
+    template = loader.get_template("sensors/data.html")
+    context = {
+        'allSensors': sensors,
+    }
+    return HttpResponse(template.render(context, request))
+
 
 def allSensors(request):
     allSensors = SensorGroup.objects.all()
     template = loader.get_template("sensors/boxes.html")
     context = {
-        'allSensors' : allSensors,
+        'allSensors': allSensors,
     }
     return HttpResponse(template.render(context, request))
+
+
+def chartTest(request):
+    template = loader.get_template("sensors/chartTest.html")
+    return HttpResponse(template.render({}, request))
+    # return render(request, 'chartTest.html', {})
