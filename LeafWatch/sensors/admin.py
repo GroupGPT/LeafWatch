356fa8967f6fa36ffe7ada15324c5bddcabddb35
PointlessUsername <42281247+PointlessUsername@users.noreply.github.com>
2023-01-07 17:56:34 -0700

    Links work now

--- LeafWatch/sensors/admin.py
+++ LeafWatch/sensors/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import SensorGroup, Sensor
+
+admin.site.register(SensorGroup)
+admin.site.register(Sensor)
 
 # Register your models here.
--- LeafWatch/sensors/urls.py
+++ LeafWatch/sensors/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.allSensors),
+    path('<str:GroupSensor_name>/', views.data),
 ]
