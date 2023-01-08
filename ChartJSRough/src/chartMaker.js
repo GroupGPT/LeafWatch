7363e21a45782889d71c38eeb79a44a4025c54c8
aflew <aflewwel@ualberta.ca>
2023-01-08 11:00:02 -0700

    combined js files into one

--- ChartJSRough/src/chartMaker.js
+++ ChartJSRough/src/chartMaker.js
@@ -1,29 +1,39 @@
 import Chart from 'chart.js/auto'
 
-var timeStamps = [];
-for (i = 0; i < 24*30; i++) {
-    timeStamps.push(i.toString());
+function generateFakeData() {
+    var timeStamps = [];
+    for (i = 0; i < 24*30; i++) {
+        timeStamps.push(i.toString());
+    }
+
+    var tempData = {};
+
+    var node1TempData = [];
+    for (i = 0; i < 24*30; i++) {
+        var temp = Math.floor(Math.random() * 80);
+        node1TempData.push(temp);
+    }
+    tempData['Node 1'] = (node1TempData);
+
+    var node2TempData = [];
+    for (i = 0; i < 24*30; i++) {
+        var temp = Math.floor(Math.random() * 80);
+        node2TempData.push(temp);
+    }
+    tempData['Node 2'] = node2TempData;
+    
+    return [timeStamps, tempData];
 }
 
-var tempData = {};
+var [ts1, td1] = generateFakeData();
+var [ts2, td2] = generateFakeData();
+var [ts3, td3] = generateFakeData();
 
-var node1TempData = [];
-for (i = 0; i < 24*30; i++) {
-    var temp = Math.floor(Math.random() * 80);
-    node1TempData.push(temp);
-}
-tempData['Node 1'] = (node1TempData);
+const ctx1 = document.getElementById('humidityChart').getContext("2d")
+const ctx2 = document.getElementById('temperatureChart').getContext("2d");
+const ctx3 = document.getElementById('lightChart').getContext("2d")
 
-var node2TempData = [];
-for (i = 0; i < 24*30; i++) {
-    var temp = Math.floor(Math.random() * 80);
-    node2TempData.push(temp);
-}
-tempData['Node 2'] = node2TempData;
-
-const ctx = document.getElementById('temperatureChart').getContext("2d");
-
-async function makeChart() {
+async function makeChart(ctx,timeStamps,tempdataset,yaxisunit) {
     var mychart = new Chart(
         ctx,
         {
@@ -49,7 +59,8 @@ async function makeChart() {
                         ticks: {
                             // Include a dollar sign in the ticks
                             callback: function (value, index, ticks) {
-                                return value + " °C";
+                                //return value + " °C";
+                                return value + " " + yaxisunit
                             }
                         }
                     }
@@ -58,16 +69,18 @@ async function makeChart() {
             },
         }
     );
-    for (key in tempData) {
-        var temp = tempData[key];
+    for (key in tempdataset) {
+        var temp = tempdataset[key];
         mychart.data.datasets.push({data: temp, label: key})
     }
-    var [newlabels, newdatasets] = newData(mychart,24,24);
+    var [newlabels, newdatasets] = newData(mychart,24,24,"hour",tempdataset);
     updatechart(mychart, newlabels, newdatasets);
     return mychart
 }
 
-var mychart = makeChart();
+var chart1 = makeChart(ctx1,ts1,td1,"%");
+var chart2 = makeChart(ctx2,ts2,td2,"°C");
+var chart3 = makeChart(ctx3,ts3,td3,"PAR");
 
 var daybutton = document.getElementById('24h');
 var weekbutton = document.getElementById('7d');
@@ -81,7 +94,7 @@ function updatechart(chart, newlabels, newdatasets) {
     chart.update();
 }
 
-function newData(chart, hours, xAxisLength, unit="hour") {
+function newData(chart, hours, xAxisLength, unit="hour", originalDataset) {
     var ratio = Math.floor(hours/xAxisLength)
     var dispRatio
     switch (unit) {
@@ -108,7 +121,7 @@ function newData(chart, hours, xAxisLength, unit="hour") {
     for (let i = 0; i < xAxisLength; i++) {
         newlabels.push(i*ratio*dispRatio);
         chart.data.datasets.forEach((dataset) => {
-            let originalData = tempData[dataset.label]
+            let originalData = originalDataset[dataset.label]
             if (ratio == 1) {
                 // why can't I just use negative indexing graahhhh
                 newdatasets[dataset.label].push(originalData[originalData.length+(i-hours)]) 
@@ -127,17 +140,35 @@ function newData(chart, hours, xAxisLength, unit="hour") {
 
 daybutton.addEventListener('click', async () => {
     //alert('please tell mme youre working')
-    let chart = await mychart
-    var [newlabels, newdatasets] = newData(chart,24,24);
-    updatechart(chart, newlabels, newdatasets);
+    let newchart1 = await chart1;
+    let newchart2 = await chart2;
+    let newchart3 = await chart3;
+    var [newlabels1, newdatasets1] = newData(newchart1,24,24,"hour",td1);
+    var [newlabels2, newdatasets2] = newData(newchart2,24,24,"hour",td2);
+    var [newlabels3, newdatasets3] = newData(newchart3,24,24,"hour",td3);
+    updatechart(newchart1, newlabels1, newdatasets1);
+    updatechart(newchart2, newlabels2, newdatasets2);
+    updatechart(newchart3, newlabels3, newdatasets3);
 });
 weekbutton.addEventListener('click', async () => {
-    let chart = await mychart
-    var [newlabels, newdatasets] = newData(chart,24*7,14,"day");
-    updatechart(chart, newlabels, newdatasets);
+    let newchart1 = await chart1
+    let newchart2 = await chart2
+    let newchart3 = await chart3
+    var [newlabels1, newdatasets1] = newData(newchart1,24*7,14,"day",td1);
+    var [newlabels2, newdatasets2] = newData(newchart2,24*7,14,"day",td2);
+    var [newlabels3, newdatasets3] = newData(newchart3,24*7,14,"day",td3);
+    updatechart(newchart1, newlabels1, newdatasets1);
+    updatechart(newchart2, newlabels2, newdatasets2);
+    updatechart(newchart3, newlabels3, newdatasets3);
 });
 monthbutton.addEventListener('click', async () => {
-    let chart = await mychart
-    var [newlabels, newdatasets] = newData(chart,24*30,30,"day");
-    updatechart(chart, newlabels, newdatasets);
+    let newchart1 = await chart1
+    let newchart2 = await chart2
+    let newchart3 = await chart3
+    var [newlabels1, newdatasets1] = newData(newchart1,24*30,30,"day",td1);
+    var [newlabels2, newdatasets2] = newData(newchart2,24*30,30,"day",td2);
+    var [newlabels3, newdatasets3] = newData(newchart3,24*30,30,"day",td3);
+    updatechart(newchart1, newlabels1, newdatasets1);
+    updatechart(newchart2, newlabels2, newdatasets2);
+    updatechart(newchart3, newlabels3, newdatasets3);
 });
